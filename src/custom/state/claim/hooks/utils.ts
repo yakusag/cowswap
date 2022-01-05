f826a5daff2bd0b10a34ea62a926c422163d2699
Leandro <leandro.boscariol@gnosis.io>
2022-01-05 11:09:06 -0800

    Refactored _repoNetworkIdMapping to use SupportedChainId enum

--- src/custom/state/claim/hooks/utils.ts
+++ src/custom/state/claim/hooks/utils.ts
@@ -6,6 +6,7 @@ import {
   RepoClaims,
   UserClaims,
 } from 'state/claim/hooks/index'
+import { SupportedChainId } from 'constants/chains'
 
 /**
  * Helper function to check whether any claim is an investment option
@@ -39,17 +40,17 @@ export function transformRepoClaimsToUserClaims(repoClaims: RepoClaims): UserCla
  * Helper function to get the repo path for the corresponding network id
  * Throws when passed an unknown network id
  */
-export function getClaimsRepoPath(id: number): string {
+export function getClaimsRepoPath(id: SupportedChainId): string {
   return `${CLAIMS_REPO}${_repoNetworkIdMapping(id)}/`
 }
 
-function _repoNetworkIdMapping(id: number): string {
+function _repoNetworkIdMapping(id: SupportedChainId): string {
   switch (id) {
-    case 1:
+    case SupportedChainId.MAINNET:
       return 'mainnet'
-    case 4:
+    case SupportedChainId.RINKEBY:
       return 'rinkeby'
-    case 100:
+    case SupportedChainId.XDAI:
       return 'gnosis-chain'
     default:
       throw new Error('Network not supported')
